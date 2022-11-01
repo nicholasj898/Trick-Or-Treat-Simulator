4a41583725a04bf1a3f3dbe1d9f9560b71452282
Nicholas Jones <70674476+nicholasj898@users.noreply.github.com>
2022-10-31 23:12:08 -0700

    Add files via upload

--- Trick Or Treat Simulator.cpp
+++ Trick Or Treat Simulator.cpp
@@ -1,14 +1,18 @@
 //Trick or Treat Simulator
+//Quick Project For Fun
+//Art by Delectable System Administrator and JGS on ascii.co.uk (https://ascii.co.uk/art/pumpkin, https://ascii.co.uk/art/candy)
 #include <iostream>
 #include <string>
 #include <stdio.h>
 #include <cstring>
 #include <fstream>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
 string mTrickOrTreat;
-string yTrickOrTreat;
+char yTrickOrTreat [30];
 const string trick = "trick";
 const string Trick = "Trick";
 const string treat = "treat";
@@ -17,7 +21,6 @@ const string tt = "trick or treat";
 const string TT = "Trick or Treat";
 const string tte = "trick or treat!";
 const string TTE = "Trick or Treat!";
-//char tt [30];
 
 void myTurn() {
   cin >> mTrickOrTreat;
@@ -28,18 +31,62 @@ void myTurn() {
     } else if (mTrickOrTreat == treat || mTrickOrTreat == Treat) {
         cout << "Ooo a treat! Thank you!" << endl;
         } else {
-            cout << "Im sorry I didnt quite catch that." << endl;
-            cout << "Please try answering with 'trick' or 'treat':" << endl;
+            cout << "... What?" << endl;
+            cout << "Please try answering with 'trick' or 'treat'" << endl;
             myTurn();
         }
 }
 
 void yourTurn() {
   cout << "Say: 'Trick or Treat!'" << endl;
-  cin >> yTrickOrTreat;
   //cin.ignore(1,'\n');
-  //cin.getline(tt, 500);
-  //getline(cin, tt);
+  //cin >> tt;
+  cin.getline(yTrickOrTreat, 500);
+  cin.getline(yTrickOrTreat, 500);
+  //cout << yTrickOrTreat <<endl;
+    
+    
+  if (yTrickOrTreat == tt || yTrickOrTreat == TT || yTrickOrTreat == tte || yTrickOrTreat == TTE) {
+    //cout << "Really a trick? I thought we were better than that." << endl;
+    srand( (unsigned)time( NULL ) );
+    if ((rand()/32768.0)*100.0<100) {
+
+cout << "Trick!" << endl;
+cout << endl;
+cout <<R"(               .,'
+                       .''.'
+                      .' .'
+           .    ' . ~,'  `.~ . `    .
+      . '  .  '   .`:_. . _:'.   `  .  ` .
+    .'   .'     ,     .'^'.    .     `.   `.
+   .    .       .A.  .     . .A.       .    .
+              .d000b.      .d000b.
+  '    '    .d0000000b.  .d0000000b.    `    `
+  .    .      .      . db  .     .      .    .
+                      d00b
+   `    `?0o.  `     `     '    '  .o0P'    '
+     .    `?00   ooooo.  .ooooo   00P' .   .
+      ` .   `?00000P ?0bd0P ?00000P' .' . '
+            . . `~~   ~~~~  .~~'  . .
+                ~     - ~~ -    ~)" << endl;    //Art by Delectable System Administrator
+
+} else {
+
+cout << "Treat!" << endl;
+
+cout << endl;
+cout <<R"(
+___      .-""-.      ___
+\  "-.  /      \  .-"  /
+ > -=.\/        \/.=- <
+ > -='/\        /\'=- <
+/__.-'  \      /  '-.__\
+         '-..-')" << endl;                      //Art by JGS
+}
+    } else {
+        cout << "No no, you're doing it wrong." << endl;
+        yourTurn();
+    }
 }
 
 int main() {
@@ -49,5 +96,6 @@ int main() {
     myTurn();
   cout << "Okay your turn." << endl;
     yourTurn();
+    cout << "Thanks for playing!" << endl;
     return 0;
 }
